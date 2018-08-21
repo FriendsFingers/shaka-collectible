b1429e52858c6395c5c845694eda8a67d73ca69f
vittominacori <vittoriominacori@gmail.com>
2018-08-21 19:59:28 +0200

    publish: imported the svg component
    generated from commit 9030f9e225847f6cc68febe86ff5ac772be4cc46

--- assets/js/5.a1ab7e19.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{167:function(n,e,t){"use strict";t.r(e);var i=t(0),l=Object(i.a)({},function(){var n=this.$createElement,e=this._self._c||n;return e("div",{staticClass:"content"},[e("ClientOnly",[e("ViewToken")],1)],1)},[],!1,null,null,null);l.options.__file="view.md";e.default=l.exports}}]);
