bdadd0872969313607560da14d16098d9a67bd76
vittominacori <vittoriominacori@gmail.com>
2018-08-25 12:31:23 +0200

    publish: improved layout and contents
    generated from commit 1683f477f5d89f80dc16abd327e14a3ad5c965e5

--- assets/js/3.820185de.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{139:function(n,w,o){}}]);
---
+++ assets/js/9.0b8db140.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{140:function(n,t,e){"use strict";e.r(t);var s=e(4),i=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"content"})},[],!1,null,null,null);i.options.__file="index.md";t.default=i.exports}}]);
