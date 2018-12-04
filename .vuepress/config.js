9ebdbf5673fb81c8ae8c20e56ae4aebf8f35d245
vittominacori <vittoriominacori@gmail.com>
2018-12-04 09:30:23 +0100

    add infura project id

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   title: 'Shaka Collectible',
   description: 'Shaka Collectibles Tokens represent a Digital Collectible issued by FriendsFingers, the Smart Contracts based Crowdfunding platform.',
+  ga: 'UA-111269229-3',
   blockchain: {
     tokenAddress: "0x4cA68712dA8DDc293E159AB2f0E6eA1D878b5949",
-    web3Provider: "https://rinkeby.infura.io",
+    web3Provider: "https://rinkeby.infura.io/v3/6a3d80dafe7a466ca65220245c7527b8",
     etherscanLink: "https://rinkeby.etherscan.io",
     networkId: "4",
     networkName: "Rinkeby Test Network",
@@ -11,14 +12,6 @@ module.exports = {
   head: [
     ['meta', { property: 'og:image', content: 'https://collectibles.friendsfingers.com/assets/images/ogimage.png' }],
     ['script', { src: 'assets/js/web3.min.js' }],
-    ['script', { async: 'async', src: 'https://www.googletagmanager.com/gtag/js?id=UA-111269229-3'}],
-    [
-      'script',
-      {},
-      "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}" +
-      "gtag('js', new Date());" +
-      "gtag('config', 'UA-111269229-3');"
-    ],
   ],
   themeConfig: {
     navbar: false,
